d642d4f31c4f47192e89b757b8c2f5a82b7c5d66
Simo0501 <sim.manna1977@gmail.com>
2024-04-20 11:47:41 +0200

    inserito const e click

--- stopandcode1/script/script.js
+++ stopandcode1/script/script.js
@@ -3,5 +3,31 @@ const numero = Math.floor(Math.random() * 10 + 1);
 
 const min = 1;
 const max = 10;
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-console.log(randomNumber);
+const randomNumber1 = Math.floor(Math.random() * 10 + 1);
+const randomNumber2 = Math.floor(Math.random() * 10 + 1);
+
+
+//const numberUser = prompt("inserisci un numero");
+
+//if (numberUser > randomNumber1) {
+  //console.log("hai vinto")
+//}
+//else {
+  //console.log("ritenta")
+//}
+
+const numberUser = document.getElementById("you");
+const numberPc = document.getElementById("cpu");
+const selectButton = document.getElementById("play-button");
+console.log(numberUser);
+console.log(numberPc);
+console.log(selectButton);
+selectButton.addEventListener("click",
+function() {
+  console.log(randomNumber1);
+  console.log(randomNumber2);
+}
+)
+
+
+
