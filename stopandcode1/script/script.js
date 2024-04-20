b214b4a2844636f0e731bc491a3a5021b305a591
Simo0501 <sim.manna1977@gmail.com>
2024-04-20 09:55:19 +0200

    inizio esercizio

---
+++ stopandcode1/script/script.js
@@ -0,0 +1,7 @@
+// ecco il codice per "estrarre" un numero random tra 1 e 10:
+const numero = Math.floor(Math.random() * 10 + 1);
+
+const min = 1;
+const max = 10;
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
